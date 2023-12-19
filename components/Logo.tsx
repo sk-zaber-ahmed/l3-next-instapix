6889a34821ef38df60a8e58da62d175de6f46b2e
Zaber Ahmed <zaber0305@gmail.com>
2023-12-19 17:00:21 +0600

    Modify: logo name

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import { Button, buttonVariants } from './ui/button';
-import Link from 'next/link';
-import { SwitchCamera } from 'lucide-react'
+import React from "react";
+import { Button, buttonVariants } from "./ui/button";
+import Link from "next/link";
+import { SwitchCamera } from "lucide-react";
 
 const Logo = () => {
-    return (
-        <Link
-            href={"/dashboard"}
-            className={buttonVariants({
-                className: "hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0",
-                variant: "ghost",
-                size: "lg",
-            })}
-        >
-            <SwitchCamera className="h-6 w-6 shrink-0 lg:hidden" />
-            <p
-                className={`font-semibold text-xl hidden lg:block`}
-            >
-                Pixelgram
-            </p>
-        </Link>
-    );
+  return (
+    <Link
+      href={"/dashboard"}
+      className={buttonVariants({
+        className:
+          "hidden md:flex navLink !mb-10 lg:hover:bg-transparent lg:!p-0",
+        variant: "ghost",
+        size: "lg",
+      })}
+    >
+      <SwitchCamera className="h-6 w-6 shrink-0 lg:hidden" />
+      <p className={`font-semibold text-xl hidden lg:block`}>Instapix</p>
+    </Link>
+  );
 };
 
 export default Logo;
