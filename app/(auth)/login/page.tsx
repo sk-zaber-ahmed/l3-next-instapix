a67c3e3cb2f27c6fd371563821fc04a801c59543
TKRchamak <tonmoykumarroy6@gmail.com>
2023-12-20 17:16:27 +0600

    change some design in login page form

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,32 +1,35 @@
-import LoginForm from '@/components/LoginForm';
-import LoginLogo from '@/components/LoginLogo';
-import React from 'react';
-import { links } from '@/lib/links';
+import LoginForm from "@/components/LoginForm";
+import LoginLogo from "@/components/LoginLogo";
+import React from "react";
+import { links } from "@/lib/links";
 
 const Login = () => {
-    return (
-        <div className="h-screen">
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-[100px] mx-6 md:mx-[100px] xl:mx-[500px]'>
-                <div className="hidden lg:block">
-                    <LoginLogo />
-                </div>
-                <div className="lg:col-span-1" >
-                    <LoginForm />
-                </div>
-            </div>
-
-            <div className='flex justify-center flex-wrap'>
-                {
-                    links?.map((link) => (
-                        <p key={link.label} className="text-sm text-gray-500 dark:text-gray-400 ml-4 mt-2 ${index !== 0 ? 'md:ml-6' : ''}`}">
-                            {link.label}
-                        </p>
-                    ))
-                }
-            </div>
-
+  return (
+    <div className="">
+      <div
+        className="grid grid-cols-1 lg:grid-cols-2 gap-x-[100px] lg:mx-[250px]"
+        style={{ border: "2px solid yellow" }}
+      >
+        <div className="hidden lg:block grid-cols-1">
+          <LoginLogo />
         </div>
-    );
+        <div className="lg:col-span-1 grid-cols-1">
+          <LoginForm />
+        </div>
+      </div>
+
+      <div className="flex justify-center flex-wrap">
+        {links?.map((link) => (
+          <p
+            key={link.label}
+            className="text-sm text-gray-500 dark:text-gray-400 ml-4 mt-2 ${index !== 0 ? 'md:ml-6' : ''}`}"
+          >
+            {link.label}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,22 +1,66 @@
 //contains all the mutation logics
+'use server';
+
+import { cookies } from "next/headers";
+
 
 export async function followUser() {
-    try {
-    } catch (error) {
-      console.log("Error while executing action", error);
-    }
+  try {
+  } catch (error) {
+    console.log("Error while executing action", error);
   }
-  export async function authenticate(
-    prevState: string | undefined,
-    formData: FormData
-  ) {
-    try {
-      // await signIn("credentials", Object.fromEntries(formData));
-    } catch (error) {
-      if ((error as Error).message.includes("CredentialsSignin"))
-        return "CredentialsSignin";
-  
-      throw error;
+}
+
+
+export async function authenticate(data: any) {
+  try {
+    const res = await fetch("http://127.0.0.1:5000/user/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const user = await res.json();
+
+    if (user?.access_token) {
+      cookies().set("access_token", user?.access_token, {
+        path: "/",
+        domain: "localhost",
+        maxAge: user?.expires_in,
+        httpOnly: true,
+        secure: false,
+      });
     }
+
+    if (user?.refresh_token) {
+      cookies().set("refresh_token", user?.refresh_token, {
+        path: "/",
+        domain: "localhost",
+        maxAge: 60 * 60 * 7,
+        httpOnly: true,
+        secure: false,
+      });
+    }
+
+    if (res.ok && user) {
+      return user;
+    } else {
+      console.log("login api not working");
+      return null;
+    }
+
+  } catch (error: any) {
+    console.log("From the authenticate function", error);
+    if (error.response) {
+      console.log(error.response?.data);
+      return error.response?.body?.error;
+    };
+
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignIn';
+    }
+    return 'authentication error';
   }
-  
+}
