ad3f21071f22a7a39f74ed227c744339fdbc9b1e
TKRchamak <tonmoykumarroy6@gmail.com>
2023-12-26 14:53:20 +0600

    update(post create dialog update FE)

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -23,14 +23,33 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Separator } from "@/components/ui/separator";
 import ImageViewCarousel from "@/components/ImageViewCarousel";
+import { ArrowLeft } from "lucide-react";
 
 function CreatePage() {
   const pathname = usePathname();
   const isCreatePage = pathname === "/dashboard/create";
   const router = useRouter();
   const mount = useMount();
-  const [imagePreviews, setImagePreviews] = useState<string[]>([]);
   const [postImage, setPostImage] = useState<File[]>([]);
+  const [stage, setStage] = useState<number>(0);
+
+  const updateStage = () => {
+    if (stage === 0) {
+      setStage(1);
+    }
+    if (stage === 1) {
+      // const formData = new FormData();
+      // if (selectedFile) {
+      //   formData.append("image", selectedFile);
+      // }
+      // const imageUrl = await uploadToStorage(formData);
+      // if (imageUrl === "cookies_not_found") {
+      //   return await uploadToStorage(formData);
+      // }
+      // console.log(imageUrl);
+      // return imageUrl;
+    }
+  };
 
   const form = useForm<z.infer<typeof CreatePost>>({
     resolver: zodResolver(CreatePost),
@@ -44,41 +63,26 @@ function CreatePage() {
     const files = event.target.files;
 
     if (files) {
-      const imagePreviewsArray: string[] = [];
       const inputImages: File[] = [];
 
       for (let i = 0; i < files.length; i++) {
-        const reader = new FileReader();
         const file = files[i];
         inputImages.push(file); // set input files
-
-        reader.readAsDataURL(file);
-
-        reader.onload = () => {
-          const base64Data = reader.result as string; // 'result' contains the base64 string
-          imagePreviewsArray.push(base64Data);
-
-          if (imagePreviewsArray.length === files.length) {
-            setImagePreviews(imagePreviewsArray);
-            setPostImage(inputImages);
-          }
-        };
       }
+
+      setPostImage(inputImages);
     }
   };
 
-  async function onSubmit(values: z.infer<typeof CreatePost>) {
-    // const formData = new FormData();
-    // if (selectedFile) {
-    //   formData.append("image", selectedFile);
-    // }
-    // const imageUrl = await uploadToStorage(formData);
-    // if (imageUrl === "cookies_not_found") {
-    //   return await uploadToStorage(formData);
-    // }
-    // console.log(imageUrl);
-    // return imageUrl;
-  }
+  const removeImage = (fileName: string) => {
+    setPostImage((prev) => prev.filter((item) => item.name !== fileName));
+  };
+
+  const updateUploadImageList = (file: File[]) => {
+    setPostImage((prev) => [...prev, ...file]);
+  };
+
+  async function onSubmit(values: z.infer<typeof CreatePost>) {}
 
   // const [userData, dispatch] = useFormState(onSubmit, undefined);
   // const { pending } = useFormStatus();
@@ -100,81 +104,121 @@ function CreatePage() {
         }}
         className="bg-background/10"
       >
-        <div className="flex flex-col border-2 rounded-lg rounded border-border bg-border w-full md:w-[70%] lg:w-[50%]">
-          <DialogHeader>
+        <div
+          className={`flex flex-col border-2 rounded-lg rounded border-border bg-border w-full md:w-[70%] 
+          ${stage === 1 ? "lg:w-[70%]" : "lg:w-[50%]"}
+          `}
+        >
+          <DialogHeader className="relative">
             <DialogTitle className="text-center text-[16px] py-2">
               Create new post
             </DialogTitle>
+
+            {postImage.length > 0 && (
+              <button
+                onClick={updateStage}
+                className="absolute right-2 top-[4px] text-[14px] text-blue-500 font-bold hover:text-blue-600 "
+              >
+                {stage === 0 ? "Next" : "Share"}
+              </button>
+            )}
+
+            {stage !== 0 && (
+              <button
+                onClick={() => setStage((prev) => prev - 1)}
+                className="absolute left-2 top-[4px] text-[14px]"
+              >
+                <ArrowLeft />
+              </button>
+            )}
           </DialogHeader>
           <Separator className="bg-foreground" />
 
-          <div className="flex items-center justify-center rounded-b-lg w-full">
-            {imagePreviews.length > 0 ? (
-              <ImageViewCarousel postImage={postImage} />
-            ) : (
-              <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="">
-                  {/* if fileurl exists then div element will be shown */}
+          <div
+            className={`flex items-center justify-center rounded-b-lg w-full`}
+          >
+            <div
+              className={`${
+                stage === 0 ? "w-full" : "lg:w-1/2 md:hidden lg:block"
+              }`}
+            >
+              {postImage.length > 0 ? (
+                <ImageViewCarousel
+                  postImage={postImage}
+                  removeImage={removeImage}
+                  updateUploadImageList={updateUploadImageList}
+                />
+              ) : (
+                <Form {...form}>
+                  <form className="">
+                    {/* if fileurl exists then div element will be shown */}
 
-                  <FormField
-                    control={form.control}
-                    name="fileUrl"
-                    render={({ field, fieldState }) => (
-                      <FormItem>
-                        <div className="flex justify-center mt-[50px]">
-                          <svg
-                            aria-label="Icon to represent media such as images or videos"
-                            className="x1lliihq x1n2onr6 x5n08af"
-                            fill="currentColor"
-                            height="77"
-                            role="img"
-                            viewBox="0 0 97.6 77.3"
-                            width="96"
-                          >
-                            <title>
-                              Icon to represent media such as images or videos
-                            </title>
-                            <path
-                              d="M16.3 24h.3c2.8-.2 4.9-2.6 4.8-5.4-.2-2.8-2.6-4.9-5.4-4.8s-4.9 2.6-4.8 5.4c.1 2.7 2.4 4.8 5.1 4.8zm-2.4-7.2c.5-.6 1.3-1 2.1-1h.2c1.7 0 3.1 1.4 3.1 3.1 0 1.7-1.4 3.1-3.1 3.1-1.7 0-3.1-1.4-3.1-3.1 0-.8.3-1.5.8-2.1z"
+                    <FormField
+                      control={form.control}
+                      name="fileUrl"
+                      render={({ field, fieldState }) => (
+                        <FormItem>
+                          <div className="flex justify-center mt-[50px]">
+                            <svg
+                              aria-label="Icon to represent media such as images or videos"
+                              className="x1lliihq x1n2onr6 x5n08af"
                               fill="currentColor"
-                            ></path>
-                            <path
-                              d="M84.7 18.4 58 16.9l-.2-3c-.3-5.7-5.2-10.1-11-9.8L12.9 6c-5.7.3-10.1 5.3-9.8 11L5 51v.8c.7 5.2 5.1 9.1 10.3 9.1h.6l21.7-1.2v.6c-.3 5.7 4 10.7 9.8 11l34 2h.6c5.5 0 10.1-4.3 10.4-9.8l2-34c.4-5.8-4-10.7-9.7-11.1zM7.2 10.8C8.7 9.1 10.8 8.1 13 8l34-1.9c4.6-.3 8.6 3.3 8.9 7.9l.2 2.8-5.3-.3c-5.7-.3-10.7 4-11 9.8l-.6 9.5-9.5 10.7c-.2.3-.6.4-1 .5-.4 0-.7-.1-1-.4l-7.8-7c-1.4-1.3-3.5-1.1-4.8.3L7 49 5.2 17c-.2-2.3.6-4.5 2-6.2zm8.7 48c-4.3.2-8.1-2.8-8.8-7.1l9.4-10.5c.2-.3.6-.4 1-.5.4 0 .7.1 1 .4l7.8 7c.7.6 1.6.9 2.5.9.9 0 1.7-.5 2.3-1.1l7.8-8.8-1.1 18.6-21.9 1.1zm76.5-29.5-2 34c-.3 4.6-4.3 8.2-8.9 7.9l-34-2c-4.6-.3-8.2-4.3-7.9-8.9l2-34c.3-4.4 3.9-7.9 8.4-7.9h.5l34 2c4.7.3 8.2 4.3 7.9 8.9z"
-                              fill="currentColor"
-                            ></path>
-                            <path
-                              d="M78.2 41.6 61.3 30.5c-2.1-1.4-4.9-.8-6.2 1.3-.4.7-.7 1.4-.7 2.2l-1.2 20.1c-.1 2.5 1.7 4.6 4.2 4.8h.3c.7 0 1.4-.2 2-.5l18-9c2.2-1.1 3.1-3.8 2-6-.4-.7-.9-1.3-1.5-1.8zm-1.4 6-18 9c-.4.2-.8.3-1.3.3-.4 0-.9-.2-1.2-.4-.7-.5-1.2-1.3-1.1-2.2l1.2-20.1c.1-.9.6-1.7 1.4-2.1.8-.4 1.7-.3 2.5.1L77 43.3c1.2.8 1.5 2.3.7 3.4-.2.4-.5.7-.9.9z"
-                              fill="currentColor"
-                            ></path>
-                          </svg>
-                        </div>
-                        <FormLabel
-                          htmlFor="picture"
-                          className="flex justify-center text-[18px]"
-                        >
-                          Drag photos and videos here
-                        </FormLabel>
-                        <FormControl>
-                          <div className="file-uploader flex justify-center">
-                            <label className="custom-file-upload m-[50px]">
-                              <input
-                                type="file"
-                                accept="image/*"
-                                multiple
-                                onChange={handleFileInputChange}
-                                style={{ display: "none" }}
-                              />
-                              Select from computer
-                            </label>
+                              height="77"
+                              role="img"
+                              viewBox="0 0 97.6 77.3"
+                              width="96"
+                            >
+                              <title>
+                                Icon to represent media such as images or videos
+                              </title>
+                              <path
+                                d="M16.3 24h.3c2.8-.2 4.9-2.6 4.8-5.4-.2-2.8-2.6-4.9-5.4-4.8s-4.9 2.6-4.8 5.4c.1 2.7 2.4 4.8 5.1 4.8zm-2.4-7.2c.5-.6 1.3-1 2.1-1h.2c1.7 0 3.1 1.4 3.1 3.1 0 1.7-1.4 3.1-3.1 3.1-1.7 0-3.1-1.4-3.1-3.1 0-.8.3-1.5.8-2.1z"
+                                fill="currentColor"
+                              ></path>
+                              <path
+                                d="M84.7 18.4 58 16.9l-.2-3c-.3-5.7-5.2-10.1-11-9.8L12.9 6c-5.7.3-10.1 5.3-9.8 11L5 51v.8c.7 5.2 5.1 9.1 10.3 9.1h.6l21.7-1.2v.6c-.3 5.7 4 10.7 9.8 11l34 2h.6c5.5 0 10.1-4.3 10.4-9.8l2-34c.4-5.8-4-10.7-9.7-11.1zM7.2 10.8C8.7 9.1 10.8 8.1 13 8l34-1.9c4.6-.3 8.6 3.3 8.9 7.9l.2 2.8-5.3-.3c-5.7-.3-10.7 4-11 9.8l-.6 9.5-9.5 10.7c-.2.3-.6.4-1 .5-.4 0-.7-.1-1-.4l-7.8-7c-1.4-1.3-3.5-1.1-4.8.3L7 49 5.2 17c-.2-2.3.6-4.5 2-6.2zm8.7 48c-4.3.2-8.1-2.8-8.8-7.1l9.4-10.5c.2-.3.6-.4 1-.5.4 0 .7.1 1 .4l7.8 7c.7.6 1.6.9 2.5.9.9 0 1.7-.5 2.3-1.1l7.8-8.8-1.1 18.6-21.9 1.1zm76.5-29.5-2 34c-.3 4.6-4.3 8.2-8.9 7.9l-34-2c-4.6-.3-8.2-4.3-7.9-8.9l2-34c.3-4.4 3.9-7.9 8.4-7.9h.5l34 2c4.7.3 8.2 4.3 7.9 8.9z"
+                                fill="currentColor"
+                              ></path>
+                              <path
+                                d="M78.2 41.6 61.3 30.5c-2.1-1.4-4.9-.8-6.2 1.3-.4.7-.7 1.4-.7 2.2l-1.2 20.1c-.1 2.5 1.7 4.6 4.2 4.8h.3c.7 0 1.4-.2 2-.5l18-9c2.2-1.1 3.1-3.8 2-6-.4-.7-.9-1.3-1.5-1.8zm-1.4 6-18 9c-.4.2-.8.3-1.3.3-.4 0-.9-.2-1.2-.4-.7-.5-1.2-1.3-1.1-2.2l1.2-20.1c.1-.9.6-1.7 1.4-2.1.8-.4 1.7-.3 2.5.1L77 43.3c1.2.8 1.5 2.3.7 3.4-.2.4-.5.7-.9.9z"
+                                fill="currentColor"
+                              ></path>
+                            </svg>
                           </div>
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </form>
-              </Form>
-            )}
+                          <FormLabel
+                            htmlFor="picture"
+                            className="flex justify-center text-[18px]"
+                          >
+                            Drag photos and videos here
+                          </FormLabel>
+                          <FormControl>
+                            <div className="file-uploader flex justify-center">
+                              <label className="custom-file-upload m-[50px]">
+                                <input
+                                  type="file"
+                                  accept="image/*"
+                                  multiple
+                                  onChange={handleFileInputChange}
+                                  style={{ display: "none" }}
+                                />
+                                Select from computer
+                              </label>
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </form>
+                </Form>
+              )}
+            </div>
+            <div
+              className={`${
+                stage === 0 ? "hidden" : "lg:w-1/2 md:w-full"
+              } h-[100px]`}
+              style={{ border: "2px solid red" }}
+            ></div>
           </div>
         </div>
       </DialogContent>
@@ -183,121 +227,3 @@ function CreatePage() {
 }
 
 export default CreatePage;
-
-// <Form {...form}>
-// <form onSubmit={form.handleSubmit(onSubmit)} className="">
-//   {/* if fileurl exists then div element will be shown */}
-//   {!!fileUrl ? (
-//     <div className="h-96 md:h-[450px] overflow-hidden rounded-md">
-//       <AspectRatio ratio={1 / 1} className="relative h-full">
-//         <Image
-//           src={fileUrl}
-//           alt="Post preview"
-//           fill
-//           className="rounded-md object-cover"
-//         />
-//       </AspectRatio>
-//     </div>
-//   ) : (
-//     <FormField
-//       control={form.control}
-//       name="fileUrl"
-//       render={({ field, fieldState }) => (
-//         <FormItem>
-//           <div className="flex justify-center mt-[50px]">
-//             <svg
-//               aria-label="Icon to represent media such as images or videos"
-//               className="x1lliihq x1n2onr6 x5n08af"
-//               fill="currentColor"
-//               height="77"
-//               role="img"
-//               viewBox="0 0 97.6 77.3"
-//               width="96"
-//             >
-//               <title>
-//                 Icon to represent media such as images or videos
-//               </title>
-//               <path
-//                 d="M16.3 24h.3c2.8-.2 4.9-2.6 4.8-5.4-.2-2.8-2.6-4.9-5.4-4.8s-4.9 2.6-4.8 5.4c.1 2.7 2.4 4.8 5.1 4.8zm-2.4-7.2c.5-.6 1.3-1 2.1-1h.2c1.7 0 3.1 1.4 3.1 3.1 0 1.7-1.4 3.1-3.1 3.1-1.7 0-3.1-1.4-3.1-3.1 0-.8.3-1.5.8-2.1z"
-//                 fill="currentColor"
-//               ></path>
-//               <path
-//                 d="M84.7 18.4 58 16.9l-.2-3c-.3-5.7-5.2-10.1-11-9.8L12.9 6c-5.7.3-10.1 5.3-9.8 11L5 51v.8c.7 5.2 5.1 9.1 10.3 9.1h.6l21.7-1.2v.6c-.3 5.7 4 10.7 9.8 11l34 2h.6c5.5 0 10.1-4.3 10.4-9.8l2-34c.4-5.8-4-10.7-9.7-11.1zM7.2 10.8C8.7 9.1 10.8 8.1 13 8l34-1.9c4.6-.3 8.6 3.3 8.9 7.9l.2 2.8-5.3-.3c-5.7-.3-10.7 4-11 9.8l-.6 9.5-9.5 10.7c-.2.3-.6.4-1 .5-.4 0-.7-.1-1-.4l-7.8-7c-1.4-1.3-3.5-1.1-4.8.3L7 49 5.2 17c-.2-2.3.6-4.5 2-6.2zm8.7 48c-4.3.2-8.1-2.8-8.8-7.1l9.4-10.5c.2-.3.6-.4 1-.5.4 0 .7.1 1 .4l7.8 7c.7.6 1.6.9 2.5.9.9 0 1.7-.5 2.3-1.1l7.8-8.8-1.1 18.6-21.9 1.1zm76.5-29.5-2 34c-.3 4.6-4.3 8.2-8.9 7.9l-34-2c-4.6-.3-8.2-4.3-7.9-8.9l2-34c.3-4.4 3.9-7.9 8.4-7.9h.5l34 2c4.7.3 8.2 4.3 7.9 8.9z"
-//                 fill="currentColor"
-//               ></path>
-//               <path
-//                 d="M78.2 41.6 61.3 30.5c-2.1-1.4-4.9-.8-6.2 1.3-.4.7-.7 1.4-.7 2.2l-1.2 20.1c-.1 2.5 1.7 4.6 4.2 4.8h.3c.7 0 1.4-.2 2-.5l18-9c2.2-1.1 3.1-3.8 2-6-.4-.7-.9-1.3-1.5-1.8zm-1.4 6-18 9c-.4.2-.8.3-1.3.3-.4 0-.9-.2-1.2-.4-.7-.5-1.2-1.3-1.1-2.2l1.2-20.1c.1-.9.6-1.7 1.4-2.1.8-.4 1.7-.3 2.5.1L77 43.3c1.2.8 1.5 2.3.7 3.4-.2.4-.5.7-.9.9z"
-//                 fill="currentColor"
-//               ></path>
-//             </svg>
-//           </div>
-//           <FormLabel
-//             htmlFor="picture"
-//             className="flex justify-center text-[18px]"
-//           >
-//             Drag photos and videos here
-//           </FormLabel>
-//           <FormControl>
-//             <div className="file-uploader flex justify-center">
-//               <label className="custom-file-upload m-[50px]">
-//                 <input
-//                   type="file"
-//                   multiple
-//                   onChange={handleFileChange}
-//                   style={{ display: "none" }}
-//                 />
-//                 Select from computer
-//               </label>
-//             </div>
-//           </FormControl>
-//           <FormMessage />
-//         </FormItem>
-//       )}
-//     />
-//   )}
-
-//   {!!fileUrl && (
-//     <>
-//       <FormField
-//         control={form.control}
-//         name="caption"
-//         render={({ field }) => (
-//           <FormItem>
-//             <FormLabel htmlFor="caption">Caption</FormLabel>
-//             <FormControl>
-//               <Input
-//                 type="caption"
-//                 id="caption"
-//                 placeholder="Write a caption..."
-//                 {...field}
-//               />
-//             </FormControl>
-//             <FormMessage />
-//           </FormItem>
-//         )}
-//       />
-//       <Button
-//         type="submit"
-//         disabled={form.formState.isSubmitting}
-//       >
-//         Create Post
-//       </Button>
-//     </>
-//   )}
-// </form>
-// </Form>
-
-// <>
-// {imagePreviews.map((preview, index) => (
-//   <Card key={index}>
-//     <CardContent className="flex aspect-square items-center justify-center p-6">
-//       <Image
-//         src={preview}
-//         alt="Post preview"
-//         fill
-//         className="rounded-md object-cover"
-//       />
-//     </CardContent>
-//   </Card>
-// ))}
-// </>
--- components/ImageViewCarousel.tsx
+++ components/ImageViewCarousel.tsx
@@ -9,32 +9,69 @@ import {
 } from "@/components/ui/carousel";
 import { CopyPlus, Plus, XCircle } from "lucide-react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 
-const ImageViewCarousel = ({ postImage }: { postImage: File[] }) => {
-  const [imagePreviews, setImagePreviews] = useState<string[]>([]);
+type extendFileType = {
+  name: string;
+  base64: string;
+};
+
+const ImageViewCarousel = ({
+  postImage,
+  removeImage,
+  updateUploadImageList,
+}: {
+  postImage: File[];
+  removeImage: (fileName: string) => void;
+  updateUploadImageList: (file: File[]) => void;
+}) => {
+  const [imagePreviews, setImagePreviews] = useState<extendFileType[]>([]);
+  const [updateOverlay, setUpdateOverlay] = useState(false);
+  // console.log(imagePreviews);
 
   const getBase64Data = (fileList: File[]) => {
     const files = fileList;
 
     if (files) {
-      const imagePreviewsArray: string[] = [];
+      const imagePreviewsArray: extendFileType[] = [];
 
       for (let i = 0; i < files.length; i++) {
         const reader = new FileReader();
         const file = files[i];
 
+        // console.log(file);
+        // let base64String = "";
+
         reader.readAsDataURL(file);
 
-        reader.onload = () => {
+        reader.onload = (event) => {
           const base64Data = reader.result as string; // 'result' contains the base64 string
-          imagePreviewsArray.push(base64Data);
+          // base64String = base64Data; // its not possible because, the result should be string | buffer
+          const newFile = { name: file?.name, base64: base64Data };
+          imagePreviewsArray.push(newFile);
 
           if (imagePreviewsArray.length === files.length) {
             setImagePreviews(imagePreviewsArray);
           }
         };
+
+        // console.log("Yes I Got the data", base64String);
       }
+    }
+  };
+
+  const getInputData = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+
+    if (files) {
+      const inputImages: File[] = [];
+
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        inputImages.push(file); // set input files
+      }
+
+      updateUploadImageList(inputImages);
     }
   };
 
@@ -46,13 +83,13 @@ const ImageViewCarousel = ({ postImage }: { postImage: File[] }) => {
     <Carousel className="rounded-b-lg">
       <CarouselContent>
         {imagePreviews.length > 0 &&
-          imagePreviews.map((item: string, index: number) => (
+          imagePreviews.map((item: extendFileType, index: number) => (
             <CarouselItem
               className="w-full h-[65vh] flex justify-center"
               key={index}
             >
               <Image
-                src={item}
+                src={item?.base64}
                 alt="Post preview"
                 className="object-cover h-[65vh] w-auto rounded-b-lg"
                 height={100}
@@ -64,36 +101,54 @@ const ImageViewCarousel = ({ postImage }: { postImage: File[] }) => {
       <CarouselPrevious className="left-5" />
       <CarouselNext className="right-5" />
 
-      <div className="absolute h-[100px] w-full  right-0 left-0 bottom-[10px] -translate-y-1/2">
-        <div className="grid grid-cols-12 gap-1 mx-4 bg-background/80 rounded-lg">
-          <div className="col-start-1 col-span-10 overflow-hidden ms-2">
-            <div className="flex items-center overflow-x-auto h-full w-full  gap-2 flex-row-reverse">
-              {imagePreviews &&
-                imagePreviews.map((item: any, index: number) => (
-                  <div className="relative flex-shrink-0 w-[90px]" key={index}>
-                    <Image
-                      src={item}
-                      alt="Post preview"
-                      className="object-cover h-[90px] w-[90px] bg-gray-200"
-                      height={90}
-                      width={90}
-                    />
-                    <button className="absolute top-1 right-1 bg-gray-800 rounded-full">
-                      <XCircle />
-                    </button>
-                  </div>
-                ))}
+      {updateOverlay && (
+        <div className="absolute h-[100px] w-full  right-0 left-0 bottom-[10px] -translate-y-1/2">
+          <div className="grid grid-cols-12 gap-1 mx-4 bg-background/80 rounded-lg">
+            <div className="col-start-1 col-span-10 overflow-hidden ms-2">
+              <div className="flex items-center overflow-x-auto h-full w-full  gap-2 flex-row-reverse">
+                {imagePreviews &&
+                  imagePreviews.map((item: any, index: number) => (
+                    <div
+                      className="relative flex-shrink-0 w-[90px]"
+                      key={index}
+                    >
+                      <Image
+                        src={item?.base64}
+                        alt="Post preview"
+                        className="object-cover h-[90px] w-[90px] bg-gray-200"
+                        height={90}
+                        width={90}
+                      />
+                      <button
+                        onClick={() => removeImage(item?.name)}
+                        className="absolute top-1 right-1 bg-gray-800 rounded-full"
+                      >
+                        <XCircle />
+                      </button>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="col-start-11 col-span-2 h-[100px] ">
+              <label className="rounded-full h-[50px] w-[50px] border-slate-50/30 border-2 flex justify-center items-center cursor-pointer">
+                <input
+                  type="file"
+                  accept="image/*"
+                  multiple
+                  onChange={getInputData}
+                  style={{ display: "none" }}
+                />
+                <Plus />
+              </label>
             </div>
           </div>
-          <div className="col-start-11 col-span-2 h-[100px] ">
-            <button className="rounded-full h-[50px] w-[50px] border-slate-50/30 border-2 flex justify-center items-center">
-              <Plus />
-            </button>
-          </div>
         </div>
-      </div>
+      )}
 
-      <button className="absolute h-8 w-8 rounded-full bg-background/80 right-[18px] bottom-[0px] -translate-y-1/2">
+      <button
+        onClick={() => setUpdateOverlay((prev) => !prev)}
+        className="absolute h-8 w-8 rounded-full bg-background/80 right-[18px] bottom-[0px] -translate-y-1/2"
+      >
         <CopyPlus className="ps-2" />
       </button>
     </Carousel>
