a727c812b365ef31ab04c211cc4022c4c011303c
Zaber Ahmed <zaber0305@gmail.com>
2023-12-27 18:07:43 +0600

    committing for nothing much

--- components/FollowersSuggestion.tsx
+++ components/FollowersSuggestion.tsx
@@ -1,30 +1,31 @@
-import React, { useState } from 'react';
-import { FollowersSuggestionCard } from './FollowersSuggestionCard';
-import { OwnProfilePage } from './OwnProfilePage';
-import { fetchSuggestedUsers } from '@/lib/data';
+import React from "react";
+import { FollowersSuggestionCard } from "./FollowersSuggestionCard";
+import { OwnProfilePage } from "./OwnProfilePage";
+import { fetchSuggestedUsers } from "@/lib/data";
 
-const FollowersSuggestion = async() => {
+const FollowersSuggestion = async () => {
+  let loggedInUser = "7e648dc6-f120-42e6-9c34-8cf366a63654";
+  const suggestedUser = await fetchSuggestedUsers(loggedInUser);
 
-    let loggedInUser="7e648dc6-f120-42e6-9c34-8cf366a63654"
-    const suggestedUser= await fetchSuggestedUsers(loggedInUser);
-    
-    return (
-        <div>
-            <OwnProfilePage></OwnProfilePage>
+  return (
+    <div>
+      <OwnProfilePage></OwnProfilePage>
 
-            <div className='flex justify-between mb-4 lg:w-[350px]'>
-                <h1 className='text-[14px] text-gray-400 font-bold'>Suggested for you</h1>
-                <h1 className='text-[13px]'>See All</h1>
-            </div>
-            {
-                suggestedUser?.suggestions?.map((suggestion:any,index:any)=>(
-                    <div key={index}>
-                        <FollowersSuggestionCard suggestion={suggestion}></FollowersSuggestionCard>
-                    </div>
-                ))
-            }
+      <div className="flex justify-between mb-4 lg:w-[350px]">
+        <h1 className="text-[14px] text-gray-400 font-bold">
+          Suggested for you
+        </h1>
+        <h1 className="text-[13px]">See All</h1>
+      </div>
+      {suggestedUser?.suggestions?.map((suggestion: any, index: any) => (
+        <div key={index}>
+          <FollowersSuggestionCard
+            suggestion={suggestion}
+          ></FollowersSuggestionCard>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default FollowersSuggestion;
