b21c2e973880d44ba50c0aa6143a41ba9fb5b046
tonmoy-kumar-roy <tonmoy.roy@selisegroup.com>
2024-01-08 17:02:58 +0600

    test

--- lib/actions.ts
+++ lib/actions.ts
@@ -283,29 +283,13 @@ export const followingUser = async (
   userToFollow: FormDataEntryValue | null
 ) => {
   try {
-    const url = `http://127.0.0.1:5000/user/follow`;
-
-    const response = await axiosInstance.post(url, {
-      loggedInUser: loggedInUser,
-      userToFollow: userToFollow,
-    };
-
-    // Define the URL for your POST request
-    const url = `http://127.0.0.1:5000/user/follow`;
-
-    // const response = await fetch(url, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //     Accept: "application/json",
-    //   },
-    //   body: JSON.stringify(formData),
-    //   cache: "no-cache",
-    // });
     if (loggedInUser && userToFollow) {
-      const response = await axiosInstance.post(url, formData);
-      revalidatePath("/dashboard");
-      return response.data;
+      const url = `http://127.0.0.1:5000/user/follow`;
+
+      const response = await axiosInstance.post(url, {
+        loggedInUser: loggedInUser,
+        userToFollow: userToFollow,
+      })
     }
   } catch (error) {
     throw new Error("Failed to fetch");
